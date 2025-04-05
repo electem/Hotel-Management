e337bf4a480b790d6d81deb23ceedc16d9f95f8c
electems <vinzy@electems.com>
2025-04-05 11:16:22 +0530

    commit for somechnge

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const nextConfig = {
-  reactStrictMode: true,
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    API_BASE_URL: process.env.API_BASE_URL
-  }
-};
-
-module.exports = nextConfig;
