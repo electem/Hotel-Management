295241a226dff027f8c172c9e0cf0265d1576a9c
electems <vinzy@electems.com>
2025-04-05 15:52:37 +0530

    remove the comment line

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   publicRuntimeConfig: {
 
     // Will be available on both server and client
-    //changes are made by vish
 
     API_BASE_URL: process.env.API_BASE_URL
   }
