67c4db62b14ff58069b8ad70cdf89fb4009caa35
electems <vinzy@electems.com>
2025-04-05 16:31:52 +0530

    added the comment line

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   publicRuntimeConfig: {
 
     // Will be available on both server and client
+    //changes are done by feature2
 
     API_BASE_URL: process.env.API_BASE_URL
   }
