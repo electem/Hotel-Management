26858012fae24010b9dd49d49faebb3d99fbe9a8
electems <vinzy@electems.com>
2025-04-05 11:25:50 +0530

    added the comment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     // Will be available on both server and client
+    //change
     API_BASE_URL: process.env.API_BASE_URL
   }
 };
